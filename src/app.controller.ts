8ba3b335f04d7a204084f5f6c3e2cccfc4ed48fc
Matheus Inocêncio <inolopesm@gmail.com>
2023-07-21 18:09:16 -0300

    initialize scrapping with puppeteer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Inject, Post } from "@nestjs/common";
 import { ClientProxy, MessagePattern, Payload } from "@nestjs/microservices";
+import puppeteer, { TimeoutError } from "puppeteer";
 import { RMQ_SERVICE } from "./tokens";
 import { CreateQueryDto } from "./create-query.dto";
 
@@ -13,7 +14,54 @@ export class AppController {
   }
 
   @MessagePattern("create-query")
-  handleMessage(@Payload() createQueryDto: CreateQueryDto) {
-    console.log("received", createQueryDto);
+  async handleMessage(@Payload() createQueryDto: CreateQueryDto) {
+    const response = await fetch("http://extratoclube.com.br/");
+    const html = await response.text();
+    const [, url] = html.match(/<frame src="([A-Za-z0-9:\/\-\.]+)"/) ?? [];
+    if (!url) throw new Error("no url found");
+
+    const browser = await puppeteer.launch({ headless: "new" });
+
+    try {
+      const page = await browser.newPage();
+
+      try {
+        await page.goto(url);
+        await page.waitForSelector("#user");
+        await page.type("#user", createQueryDto.username);
+        await page.waitForSelector("#pass");
+        await page.type("#pass", createQueryDto.password);
+        await page.waitForSelector("#botao");
+
+        try {
+          await Promise.all([
+            page.click("#botao"),
+            page.waitForNavigation({ timeout: 5000 }),
+          ]);
+        } catch (error) {
+          if (error instanceof TimeoutError) {
+            const error = await page.evaluate(
+              () =>
+                document.querySelector<HTMLDivElement>("#alert-1-msg")
+                  ?.innerText
+            );
+
+            if (error) throw new Error(error);
+
+            throw new Error("wait for 5s and #alert-1-msg not found", {
+              cause: error,
+            });
+          }
+
+          throw error;
+        }
+      } finally {
+        page.close();
+      }
+
+      console.log("received", createQueryDto);
+    } finally {
+      await browser.close();
+    }
   }
 }
