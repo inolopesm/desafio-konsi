12f335c70932b50f34a5f7edf21af3c84e7f3b78
Matheus Inocêncio <inolopesm@gmail.com>
2023-07-24 10:13:39 -0300

    create tests todos for submit form operations

---
+++ src/beneficios/operations/submit-login-form.puppeteer-page-operation.spec.ts
@@ -0,0 +1,9 @@
+describe("SubmitLoginFormPuppeteerPageOperation", () => {
+  it.todo("should submit the form clicking in button");
+  it.todo("should wait for navigation");
+  it.todo("should get the error message if the page has not navigated");
+  it.todo("should throw an exception if message has found");
+  it.todo("should throw an error if message has not found");
+  it.todo("should throw if waitForNavigation throws and not TimeoutError");
+  it.todo("should return the same data it received");
+});
