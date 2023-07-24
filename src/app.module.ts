41e544b5ce910dee963588d74b1afed085f02350
Matheus Inocêncio <inolopesm@gmail.com>
2023-07-24 18:37:37 -0300

    configure cache-manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { ClientProxyFactory, Transport } from "@nestjs/microservices";
 import { BeneficiosModule } from "./beneficios";
 import { AppController } from "./app.controller";
 import { RMQ_SERVICE } from "./tokens";
+import { CacheModule } from "@nestjs/cache-manager";
+import { RedisCacheStore } from "./redis.cache-store";
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), BeneficiosModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const url = configService.getOrThrow("REDIS_URL");
+        const redisCacheStore = new RedisCacheStore();
+        await redisCacheStore.connect(url);
+        return { store: redisCacheStore };
+      },
+    }),
+    BeneficiosModule,
+  ],
   providers: [
     {
       provide: RMQ_SERVICE,
