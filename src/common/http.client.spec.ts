69bf348e034bc671166536ef58fde38141161fe6
Matheus Inocêncio <matheus.inocencio@novaescola.org.br>
2023-07-29 17:37:56 -0300

    create tests to do in http client

---
+++ src/common/http.client.spec.ts
@@ -0,0 +1,8 @@
+describe("HttpClient", () => {
+  it.todo("should fetch with correct params");
+  it.todo("should convert body if content type is json");
+  it.todo("should throws an error with status if response is not ok");
+  // prettier-ignore
+  it.todo("should throws an error with message if response is not ok and have message in body");
+  it.todo("should return response and body when success");
+});
