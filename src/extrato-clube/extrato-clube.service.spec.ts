c259094dbe21f9fe9a1ec955526d90c176d3b973
Matheus Inocêncio <matheus.inocencio@novaescola.org.br>
2023-07-29 17:52:39 -0300

    create tests to do in extrato clube service

---
+++ src/extrato-clube/extrato-clube.service.spec.ts
@@ -0,0 +1,17 @@
+describe("ExtratoClubeService", () => {
+  describe("login", () => {
+    it.todo("should make a request to login route in external api");
+    it.todo("should return an error if authorization has not found");
+    it.todo("should return the authorization header when success");
+    it.todo("should return the error if instanceof HttpClientError");
+    it.todo("should throw error in others cases");
+  });
+
+  describe("findNBs", () => {
+    it.todo("should make a request to listagem route in external api");
+    it.todo("should return an error if no nb was found");
+    it.todo("should return nbs when success");
+    it.todo("should return the error if instanceof HttpClientError");
+    it.todo("should throw error in others cases");
+  });
+});
