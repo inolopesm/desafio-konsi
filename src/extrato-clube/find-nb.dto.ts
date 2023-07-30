0ecd7df94d2f579b9fc66be45949cc612ec5633f
Matheus Inocêncio <inolopesm@gmail.com>
2023-07-30 13:17:55 -0300

    improve cpf validation message

--- src/extrato-clube/find-nb.dto.ts
+++ src/extrato-clube/find-nb.dto.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty, IsString, Matches } from "class-validator";
 
 /** Find Número do Benefício (NBs) DTO */
 export class FindNBDto {
-  @Matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)
+  @Matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, { message: "cpf must be a CPF" })
   @IsString()
   readonly cpf!: string;
 
--- src/extrato-clube/find-nbs.dto.ts
+++ src/extrato-clube/find-nbs.dto.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty, IsString, Matches } from "class-validator";
 
 /** Find Números dos Benefícios (NBs) DTO */
 export class FindNBsDto {
-  @Matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)
+  @Matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, { message: "cpf must be a CPF" })
   @IsString()
   readonly cpf!: string;
 
