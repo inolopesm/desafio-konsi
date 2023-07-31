00c2713d450e0a5bd87be132675f88349290e562
Matheus Inocêncio <inolopesm@gmail.com>
2023-07-31 09:08:57 -0300

    cover findNBs method in ExtratoClubeService with tests

--- src/extrato-clube/extrato-clube.service.spec.ts
+++ src/extrato-clube/extrato-clube.service.spec.ts
@@ -14,7 +14,7 @@ describe("ExtratoClubeService", () => {
   });
 
   describe("login", () => {
-    it("should make a request to login route in external api", async () => {
+    it("should make a request to correct route in external api", async () => {
       const Authorization = "Auth " + Math.random().toString().substring(2);
       const response = { headers: new Headers({ Authorization }), data: null };
 
@@ -71,10 +71,67 @@ describe("ExtratoClubeService", () => {
   });
 
   describe("findNBs", () => {
-    it.todo("should make a request to listagem route in external api");
-    it.todo("should return an error if no nb was found");
-    it.todo("should return nbs when success");
-    it.todo("should return the error if instanceof HttpClientError");
-    it.todo("should throw error in others cases");
+    it("should make a request to correct route in external api", async () => {
+      const authorization = "Auth " + Math.random().toString().substring(2);
+      const cpf = "000.000.000-" + Math.ceil(Math.random() * 100);
+
+      const response = { headers: new Headers(), data: { beneficios: [] } };
+      const requestSpy = jest.spyOn(HttpClient, "request");
+      requestSpy.mockResolvedValueOnce(response);
+
+      await service.findNBs({ authorization, cpf });
+
+      expect(requestSpy).toHaveBeenCalledWith(
+        `http://extratoblubeapp-env.eba-mvegshhd.sa-east-1.elasticbeanstalk.com/offline/listagem/${cpf}`,
+        { headers: { authorization } }
+      );
+    });
+
+    it("should return an error if no nb was found", async () => {
+      const [authorization, cpf] = ["Auth token", "000.000.000-00"];
+      const error = new Error("No NB was found in this CPF");
+
+      const beneficios = [{ nb: "Matrícula não encontrada!" }];
+      const response = { headers: new Headers(), data: { beneficios } };
+      const requestSpy = jest.spyOn(HttpClient, "request");
+      requestSpy.mockResolvedValueOnce(response);
+
+      const result = await service.findNBs({ authorization, cpf });
+      expect(result).toEqual(error);
+    });
+
+    it("should return nbs when success", async () => {
+      const [authorization, cpf] = ["Auth token", "000.000.000-00"];
+
+      const beneficios = [{ nb: "123456" }];
+      const response = { headers: new Headers(), data: { beneficios } };
+      const requestSpy = jest.spyOn(HttpClient, "request");
+      requestSpy.mockResolvedValueOnce(response);
+
+      const result = await service.findNBs({ authorization, cpf });
+      expect(result).toEqual({ nbs: beneficios.map(({ nb }) => nb) });
+    });
+
+    it("should return the error if instanceof HttpClientError", async () => {
+      const [authorization, cpf] = ["Auth token", "000.000.000-00"];
+      const error = new HttpClientError("random error");
+
+      const requestSpy = jest.spyOn(HttpClient, "request");
+      requestSpy.mockRejectedValueOnce(error);
+
+      const result = await service.findNBs({ authorization, cpf });
+      expect(result).toEqual(error);
+    });
+
+    it("should throw error in others cases", async () => {
+      const [authorization, cpf] = ["Auth token", "000.000.000-00"];
+      const error = new Error("unexpected error");
+
+      const requestSpy = jest.spyOn(HttpClient, "request");
+      requestSpy.mockRejectedValueOnce(error);
+
+      const promise = service.findNBs({ authorization, cpf });
+      await expect(promise).rejects.toThrow(error);
+    });
   });
 });
